10eace39409b7a7abe59e936ed6c2aca4f4a0e62
kulsumansari <kulsumansari4@gmail.com>
2024-11-29 00:22:41 +0530

    folders renamed

--- ContentSpock/handlers/reportIssue.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const reportBtn = document.getElementById("reportIssueBtn");
-
-  reportBtn.addEventListener("click", function () {
-    alert("To report an issue, please add video, audio, or text.");
-  });
-});
